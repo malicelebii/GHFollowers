3cf1ec3ebc4d66223513d390bde7437ab55d1317
malicelebii <mehmetalicelebii@gmail.com>
2024-09-23 13:38:44 +0300

    + Configure Logo

--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -8,14 +8,30 @@
 import UIKit
 
 class SearchVC: UIViewController {
-
+    
+    let logoImageView = UIImageView()
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        configureLogoImageView()
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.isNavigationBarHidden = true
     }
+    
+    func configureLogoImageView() {
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "gh-logo")
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200),
+        ])
+    }
+    
     }
 }
