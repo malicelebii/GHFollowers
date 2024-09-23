13386eb32026a565347fa30c7d43e6fc49356a2d
malicelebii <mehmetalicelebii@gmail.com>
2024-09-23 13:38:24 +0300

    + Change view background, + Hide navigation bar before view did appear

--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -11,6 +11,11 @@ class SearchVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        view.backgroundColor = .systemBackground
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
     }
 }
