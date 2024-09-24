6f0f80241ca07b412c402b5023e964f2d2b34963
malicelebii <mehmetalicelebii@gmail.com>
2024-09-24 06:08:25 +0300

    + Create Followers VC and username variable

---
+++ GHFollowers/FollowersVC.swift
@@ -0,0 +1,20 @@
+//
+//  FollowersVC.swift
+//  GHFollowers
+//
+//  Created by Mehmet Ali ÇELEBİ on 24.09.2024.
+//
+
+import UIKit
+
+class FollowersVC: UIViewController {
+
+    var username: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationController?.isNavigationBarHidden = false
+    }
+    
+}
