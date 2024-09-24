1db35d8560c4ffa5965c6d38160bfbecfcdc8f73
malicelebii <mehmetalicelebii@gmail.com>
2024-09-24 08:53:11 +0300

    + Add containerView to wrap up all components and configure it

--- GHFollowers/GFAlertVC.swift
+++ GHFollowers/GFAlertVC.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class GFAlertVC: UIViewController {
 
+    let containerView = UIView()
     var alertTitle: String?
     var message: String?
     var buttonTitle: String?
@@ -27,6 +28,23 @@ class GFAlertVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.7)
+        configureContainerView()
+    }
+    
+    func configureContainerView() {
+        view.addSubview(containerView)
+        containerView.backgroundColor = .systemBackground
+        containerView.layer.cornerRadius = 16
+        containerView.layer.borderWidth = 2
+        containerView.layer.borderColor = UIColor.white.cgColor
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            containerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            containerView.widthAnchor.constraint(equalToConstant: 280),
+            containerView.heightAnchor.constraint(equalToConstant: 280),
+        ])
     }
     
     }
