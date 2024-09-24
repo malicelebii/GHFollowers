910d9874e4c5a3262b684598cd557ce0b4e3a431
malicelebii <mehmetalicelebii@gmail.com>
2024-09-24 05:41:39 +0300

    + Edit GFTextField returnKeyTpe to go

--- GHFollowers/GFTextField.swift
+++ GHFollowers/GFTextField.swift
@@ -32,6 +32,7 @@ class GFTextField: UITextField {
         
         backgroundColor = .tertiarySystemBackground
         autocorrectionType = .no
+        returnKeyType = .go
         
         placeholder = "Enter a username"
     }
