7483f865935dc8a2e7854f64f5222626dba1ec7e
malicelebii <mehmetalicelebii@gmail.com>
2024-09-25 05:54:04 +0300

    + Set navigationBarHidden to false in willapper method

--- GHFollowers/Modules/Followers/FollowersVC.swift
+++ GHFollowers/Modules/Followers/FollowersVC.swift
@@ -14,7 +14,10 @@ class FollowersVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        navigationController?.isNavigationBarHidden = false
     }
-    
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
 }
