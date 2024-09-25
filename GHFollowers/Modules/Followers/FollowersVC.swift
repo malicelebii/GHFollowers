714cfd619bb64835188205ac8f54c47554f0025a
malicelebii <mehmetalicelebii@gmail.com>
2024-09-25 08:54:23 +0300

    + Initialize followers collection view

--- GHFollowers/Modules/Followers/FollowersVC.swift
+++ GHFollowers/Modules/Followers/FollowersVC.swift
@@ -10,14 +10,31 @@ import UIKit
 class FollowersVC: UIViewController {
 
     var username: String?
+    var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        
+        var collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.identifier)
+        return collectionView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+        setupUI()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    private func setupUI() {
+        view.addSubview(collectionView)
+        collectionView.frame = view.bounds
+        collectionView.backgroundColor = .systemPink
     }
 }
