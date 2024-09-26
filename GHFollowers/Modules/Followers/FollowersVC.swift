63a363b2b47d8d56e9231ea051a738df9aea7186
malicelebii <mehmetalicelebii@gmail.com>
2024-09-26 22:15:48 +0300

    + Convert collection view to collection view diffable data source

--- GHFollowers/Modules/Followers/FollowersVC.swift
+++ GHFollowers/Modules/Followers/FollowersVC.swift
@@ -9,16 +9,18 @@ import UIKit
 
 class FollowersVC: UIViewController {
 
+    enum Section {
+        case main
+    }
+
     var username: String?
     var collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .vertical
-        
-        var collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        var collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.identifier)
         return collectionView
     }()
+    var collectionViewDiffableDataSource: UICollectionViewDiffableDataSource<Section, Follower>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
