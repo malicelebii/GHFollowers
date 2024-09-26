a183924d5b2bb6e835791ff370fada75391d902c
malicelebii <mehmetalicelebii@gmail.com>
2024-09-26 22:29:06 +0300

    + Create FollowersViewModel to manage FollowersVC + Inject network manager dependency + Write getFollowers and updateData methods

---
+++ GHFollowers/Modules/Followers/FollowersViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  FollowersViewModel.swift
+//  GHFollowers
+//
+//  Created by Mehmet Ali ÇELEBİ on 26.09.2024.
+//
+
+import UIKit
+
+protocol FollowersViewModelProtocol {
+    func updateData()
+    func getFollowers(for username: String?)
+}
+
+final class FollowersViewModel: FollowersViewModelProtocol {
+    var followers: [Follower] = []
+    let networkManager: NetworkManagerProtocol
+    
+    init(networkManager: NetworkManagerProtocol = NetworkManager.shared) {
+        self.networkManager = networkManager
+    }
+    
+    func updateData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(followers)
+//        DispatchQueue.main.async {
+//            self.collectionViewDiffableDataSource.apply(snapshot, animatingDifferences: true , completion: nil)
+//        }
+    }
+    
+    func getFollowers(for username: String?) {
+        guard let username else { return }
+        networkManager.getFollowers(for: username, page: 1) { result in
+            switch result {
+            case .success(let followers):
+                self.followers = followers
+                self.updateData()
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
