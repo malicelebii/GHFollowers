6c05cc8e58f7b95cb263acac2cad2f5c4e845451
malicelebii <mehmetalicelebii@gmail.com>
2024-10-01 21:37:50 +0300

    + Write showLoadingIndicator func to show indicator while loading something

--- GHFollowers/Extensions/UIViewController+Extension.swift
+++ GHFollowers/Extensions/UIViewController+Extension.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+fileprivate var container: UIView!
+
 extension UIViewController {
     func presentGFAlert(title: String, message: String, buttonTitle: String) {
         DispatchQueue.main.async {
@@ -15,5 +17,22 @@ extension UIViewController {
             alert.modalTransitionStyle = .crossDissolve
             self.present(alert, animated: true, completion: nil)
         }
+    }
+    
+    func showLoadingIndicator() {
+        container = UIView(frame: view.bounds)
+        view.addSubview(container)
+        container.backgroundColor = .systemBackground
+        container.alpha = 0
+        
+        UIView.animate(withDuration: 0.3) {
+            container.alpha = 0.8
+        }
+        
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        activityIndicator.color = .label
+        container.addSubview(activityIndicator)
+        activityIndicator.center = container.center
+        activityIndicator.startAnimating()
     }
 }
