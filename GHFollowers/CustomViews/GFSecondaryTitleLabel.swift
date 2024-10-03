d95c98eb537a91a5afe3ef30722d17aaa93a3ba1
malicelebii <mehmetalicelebii@gmail.com>
2024-10-03 10:58:49 +0300

    + Create custom secondary title label

---
+++ GHFollowers/CustomViews/GFSecondaryTitleLabel.swift
@@ -0,0 +1,34 @@
+//
+//  GFSecondaryTitleLabel.swift
+//  GHFollowers
+//
+//  Created by Mehmet Ali ÇELEBİ on 3.10.2024.
+//
+
+
+import UIKit
+
+class GFSecondaryTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(fontSize: CGFloat) {
+        super.init(frame: .zero)
+        self.font = .systemFont(ofSize: fontSize, weight: .medium)
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .secondaryLabel
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.90
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
