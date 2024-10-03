e67c24018f8c87c0c2e01a1694ed56632510b938
malicelebii <mehmetalicelebii@gmail.com>
2024-10-03 10:43:07 +0300

    + Misspelling bug fix the some of user model properties that cause decoding error

--- GHFollowers/Models/User.swift
+++ GHFollowers/Models/User.swift
@@ -9,13 +9,13 @@ import Foundation
 
 struct User: Codable {
     let login: String
-    let avatarURL: String
+    let avatarUrl: String
     var name: String?
     var location: String?
     var bio: String?
     let publicRepos: Int
     let publicGists: Int
-    let htmlURL: String
+    let htmlUrl: String
     let following: Int
     let followers: Int
     let createdAt: String
