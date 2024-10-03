d99835a75f32dda9459cb877eb301fec8a74b182
malicelebii <mehmetalicelebii@gmail.com>
2024-10-03 10:41:55 +0300

    + Write getUser method inside network manager to fetch user info from api

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -9,6 +9,7 @@ import UIKit
 
 protocol NetworkManagerProtocol {
     func getFollowers(for username: String, page: Int, completion: @escaping (Result<[Follower], NetworkErrors>) -> Void)
+    func getUser(for username: String, completion: @escaping (Result<User, NetworkErrors>) -> Void)
     func downloadImage(from urlString: String) async -> UIImage
 }
 
@@ -45,6 +46,41 @@ final class NetworkManager: NetworkManagerProtocol {
             do {
                 let followers = try decoder.decode([Follower].self, from: data)
                 completion(.success(followers))
+            } catch {
+                completion(.failure(.decodingError))
+            }
+        }
+        task.resume()
+    }
+    
+    func getUser(for username: String, completion: @escaping (Result<User, NetworkErrors>) -> Void) {
+        let endpoint = baseURL + username + "asd"
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        guard let url = URL(string: endpoint) else {
+            completion(.failure(.invalidURL))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let _ = error {
+                completion(.failure(.invalidRequest))
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let data else {
+                completion(.failure(.invalidData))
+                return
+            }
+         
+            do {
+                let user = try decoder.decode(User.self, from: data)
+                completion(.success(user))
             } catch {
                 completion(.failure(.decodingError))
             }
