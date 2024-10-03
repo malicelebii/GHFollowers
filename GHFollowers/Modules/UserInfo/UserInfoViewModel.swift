a6223f3a3d1681cbe6ec20bc5a45e7783af74f0c
malicelebii <mehmetalicelebii@gmail.com>
2024-10-03 10:44:05 +0300

    + Create UserInfoViewModel and its protocol with getUserInfo method by calling necessary method from injected network manager

---
+++ GHFollowers/Modules/UserInfo/UserInfoViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  UserInfoViewModel.swift
+//  GHFollowers
+//
+//  Created by Mehmet Ali ÇELEBİ on 3.10.2024.
+//
+
+protocol UserInfoViewModelProtocol {
+    var view: UserInfoViewDelegate? { get set}
+    func getUserInfo()
+}
+
+final class UserInfoViewModel: UserInfoViewModelProtocol {
+    let networkManager: NetworkManagerProtocol
+    let username: String
+    weak var view: UserInfoViewDelegate?
+    
+    init(networkManager: NetworkManagerProtocol = NetworkManager.shared, username: String) {
+        self.networkManager = networkManager
+        self.username = username
+        getUserInfo()
+    }
+    
+    func getUserInfo() {
+        networkManager.getUser(for: username) {[weak self] result in
+            guard let self else { return }
+            switch result {
+            case .success(let user):
+                print(user)
+            case .failure(let error):
+                self.view?.showAlert(with: error.rawValue)
+            }
+        }
+    }
+}
