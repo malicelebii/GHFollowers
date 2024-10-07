7fe7fb037ca8903b3e5c718a4c8ddbce11e88208
malicelebii <mehmetalicelebii@gmail.com>
2024-10-07 09:38:17 +0300

    + Configure repo items

---
+++ GHFollowers/CustomViews/GFRepoItemVC.swift
@@ -0,0 +1,21 @@
+//
+//  GFRepoItemVC.swift
+//  GHFollowers
+//
+//  Created by Mehmet Ali ÇELEBİ on 7.10.2024.
+//
+
+import UIKit
+
+class GFRepoItemVC: GFItemInfoVC {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .repos, withCount: user.publicRepos)
+        itemInfoViewTwo.set(itemInfoType: .gists, withCount: user.publicGists)
+        actionButton.set(backgroundColor: .systemPurple, title: "Github Profile")
+    }
+}
