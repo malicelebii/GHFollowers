138160fa27c66eebb8c36ec77aab13120848c945
malicelebii <mehmetalicelebii@gmail.com>
2024-10-07 09:30:59 +0300

    + Edit item info vc by adding user as initializer parameter

--- GHFollowers/CustomViews/GFItemInfoVC.swift
+++ GHFollowers/CustomViews/GFItemInfoVC.swift
@@ -14,6 +14,17 @@ class GFItemInfoVC: UIViewController {
     let itemInfoViewTwo = GFItemInfoView()
     let actionButton = GFButton()
     
+    let user: User
+    
+    init(user: User) {
+        self.user = user
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureBackgrounView()
