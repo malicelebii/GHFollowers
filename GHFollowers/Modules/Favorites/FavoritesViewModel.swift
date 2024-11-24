cd7b05f114882da55d3939d13308499ecd514e53
malicelebii <mehmetalicelebii@gmail.com>
2024-11-24 19:34:09 +0300

    + Write fetchFavorites and deleteFavorites methods

--- GHFollowers/Modules/Favorites/FavoritesViewModel.swift
+++ GHFollowers/Modules/Favorites/FavoritesViewModel.swift
@@ -1,3 +1,4 @@
+//
 //  FavoritesViewModel.swift
 //  GHFollowers
 //
@@ -18,6 +19,33 @@ protocol FavoritesViewModelProtocol {
 final class FavoritesViewModel: FavoritesViewModelProtocol {
     var favorites: [Follower] = []
     weak var delegate: FavoritesListDelegate?
+    
+    func fetchFavorites() {
+        PersistanceManager.retrieveFavorites {[weak self] result in
+            guard let self else { return }
+            switch result {
+            case .success(let favorites):
+                self.favorites = favorites
+                self.delegate?.didChangeFavoriteList()
+            case .failure:
+                break
+            }
+        }
+    }
+    
+    func deleteFavorite(favorite: Follower) {
+        PersistanceManager.updateWith(favorite: favorite, actionType: .remove) {[weak self] result in
+            switch result {
+            case .success:
+                self?.favorites.removeAll(where: { $0.login == favorite.login })
+                self?.delegate?.didChangeFavoriteList()
+                break
+            case .failure:
+                break
+            }
+        }
+    }
+}
 
 extension FavoritesViewModel {
     func numberOfRowsInSection() -> Int {
